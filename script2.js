1f3c9d64b3800c38f4bf9bb38df34b46bc2ed608
rahul <nizarerahul7@gmail.com>
2024-12-19 22:33:11 +0530

    new changes

--- script2.js
+++ script2.js
@@ -1,14 +1,14 @@
-const productsBody = document.querySelector(".products-body");
+const productHtml = document.querySelector(".products-body");
 const productsCart = [];
 fetch("https://dummyjson.com/products")
-  .then((res) => res.json())
-  .then((data) => {
+.then((res) => res.json())
+.then((data) => {
     fetchDataObj = data.products;
     // console.log(fetchDataObj)
     fetchDataObj.forEach((e) => {
       const { id, title, description, images, rating, price } = e;
-      productsBody.innerHTML += `
-        <div class="card col" style="width: 18rem">
+      productHtml.innerHTML += `
+        <div class="card col" id=${id} style="width: 18rem">
             <img
             src= ${images[0]} 
               class="card-img-top"
@@ -23,23 +23,49 @@ fetch("https://dummyjson.com/products")
               <p>Rating : <span class="rating">${rating}</span>
               </p>
               <div class="cart-count row mx-auto input-group border border-dark rounded-3">
-              <button class="col btn btn-primary ${id} rmCount">-</button>
+              <button class="col btn btn-primary rmCount">-</button>
               <h4 class="col-8 text-center Itemquntity">0</h4>
-              <button class="col btn btn-primary ${id} addCount">+</button>
+              <button class="col btn btn-primary addCount">+</button>
               </div>
               </div>
               </div>`;
-              increment(title,images,price);
+    });
+    const addCounts = document.querySelectorAll(".addCount");
+    updateCartItme(addCounts);
+  });
+
+function updateCartItme(addCounts) {
+  addCounts.forEach((e) => {
+    e.addEventListener("click", (x) => {
+      const positionItem = x.target.parentElement.parentElement.parentElement;
+      const isAvailable = productsCart.find((p)=>{
+        return p.id == positionItem.id
+      })
+      console.log(isAvailable)
+      
+      const itemTocart = {
+          id: positionItem.id,
+          quantity: 1,
+        };
+        productsCart.push(itemTocart)
+      console.log(productsCart)
     });
   });
-function increment(title,images,price) {
-  const addCounts = document.querySelectorAll(".addCount");
-  addCounts.forEach((e) => {
-    const addProductDeatils = {
-        ptitle : title,
-        pimg : images[0],
-        pprice : price 
-    }
-    console.log(addProductDeatils)
+}
+
+
+
+
+
+function DisplayCartPage() {
+  const cartPage = document.querySelector(".cart-page");
+  const closeBtn = document.querySelector(".close-btn");
+  const cartBtn = document.querySelector(".cart-btn");
+  cartBtn.addEventListener("click", () => {
+    cartPage.style.transform = "translateX(0)";
+  });
+  closeBtn.addEventListener("click", () => {
+    cartPage.style.transform = "translateX(100%)";
   });
 }
+DisplayCartPage();
